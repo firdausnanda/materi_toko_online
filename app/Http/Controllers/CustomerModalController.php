f24e792bb0bbe824ed80f7c72680f2337ffa82ab
firdausnanda <firdausnanda46@gmail.com>
2023-06-03 12:50:52 +0700

    [FIX] Modal Validation

--- app/Http/Controllers/CustomerModalController.php
+++ app/Http/Controllers/CustomerModalController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerModalController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $customer = Customer::with('order')->get();
+
+        if ($request->ajax()) {
+            return response()->json([
+                'status' => 'success',
+                'data' => $customer
+            ]);
+        }
+
         return view('master.customer_modal.customer', compact('customer'));
     }
 
@@ -20,13 +29,26 @@ class CustomerModalController extends Controller
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'nama' => 'required|max:255',
+            'email' => 'required',
+            'alamat' => 'required',
+        ]);
+ 
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
         $customer = Customer::create([
             'nama' => $request->nama,
             'email' => $request->email,
             'alamat' => $request->alamat,
         ]);
 
-        return redirect('/modal/customer');
+        return response()->json([
+            'status' => 'success',
+            'data' => $customer
+        ]);
     }
 
     public function edit($id)
@@ -40,6 +62,16 @@ class CustomerModalController extends Controller
 
     public function update(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'nama' => 'required|max:255',
+            'email' => 'required',
+            'alamat' => 'required',
+        ]);
+ 
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
         $customer = Customer::find($request->id)->update([
             'nama' => $request->nama,
             'email' => $request->email,
--- resources/views/master/customer_modal/customer.blade.php
+++ resources/views/master/customer_modal/customer.blade.php
@@ -4,7 +4,8 @@
     <h1>Data Customer</h1>
 
     <div class="table-responsive">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-create">Tambah Data</button>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-create">Tambah
+            Data</button>
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -13,7 +14,7 @@
                     <th scope="col" class="text-center">Aksi</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody id="tableBody">
                 @forelse ($customer as $c)
                     <tr class="">
                         <td scope="row">{{ $loop->iteration }}.</td>
@@ -43,29 +44,39 @@
     </div>
 
     {{-- Modal Tambah Data --}}
-    <div class="modal fade" id="modal-create" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
-        aria-labelledby="modalTitleId" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
+    <div class="modal fade" id="modal-create" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false"
+        role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="modalTitleId">Modal Edit</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form action="{{ url('/modal/customer-store') }}" method="post">
+                <div class="container mt-2" id="alert-store"></div>
+                <form id="form-tambah">
                     <div class="modal-body">
                         @csrf
                         <div class="mb-3">
                             <label for="">Nama</label>
                             <input type="hidden" name="id" class="form-control">
-                            <input type="text" name="nama" class="form-control">
+                            <input type="text" name="nama" id="store_nama" class="form-control">
+                            <span class="invalid-feedback">
+                                <strong id="nama_msg_store"></strong>
+                            </span>
                         </div>
                         <div class="mb-3">
                             <label for="">Email</label>
-                            <input type="text" name="email" class="form-control">
+                            <input type="text" name="email" id="store_email" class="form-control">
+                            <span class="invalid-feedback">
+                                <strong id="email_msg_store"></strong>
+                            </span>
                         </div>
                         <div class="mb-3">
                             <label for="">Alamat</label>
-                            <input type="text" name="alamat" class="form-control">
+                            <input type="text" name="alamat" id="store_alamat" class="form-control">
+                            <span class="invalid-feedback">
+                                <strong id="alamat_msg_store"></strong>
+                            </span>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -80,28 +91,37 @@
     {{-- Modal Edit --}}
     <div class="modal fade" id="modal-edit" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
         aria-labelledby="modalTitleId" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="modalTitleId">Modal Edit</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form action="{{ url('/modal/customer-update') }}" method="post">
+                <div class="container mt-2" id="alert-edit"></div>
+                <form id="form-edit">
                     <div class="modal-body">
-                        @method('put')
                         @csrf
                         <div class="mb-3">
                             <label for="">Nama</label>
                             <input type="hidden" name="id" id="id" class="form-control">
                             <input type="text" name="nama" id="nama" class="form-control">
+                            <span class="invalid-feedback">
+                                <strong id="nama_msg"></strong>
+                            </span>
                         </div>
                         <div class="mb-3">
                             <label for="">Email</label>
                             <input type="text" name="email" id="email" class="form-control">
+                            <span class="invalid-feedback">
+                                <strong id="email_msg"></strong>
+                            </span>
                         </div>
                         <div class="mb-3">
                             <label for="">Alamat</label>
                             <input type="text" name="alamat" id="alamat" class="form-control">
+                            <span class="invalid-feedback">
+                                <strong id="alamat_msg"></strong>
+                            </span>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -117,6 +137,36 @@
 @section('script')
     <script>
         $(document).ready(function() {
+
+            function reloadTable() {
+                $.ajax({
+                    url: "{{ url('/modal/customer') }}",
+                    method: 'GET',
+                    beforeSend: function(param) {
+                        $('#tableBody').empty();
+                    },
+                    success: function(response) {
+                        $.each(response.data, function(i, v) {
+                            // select row
+                            var newRow = $('<tr>');
+
+                            // tambah kolom table
+                            newRow.append($('<td>').text(i + 1));
+                            newRow.append($('<td>').text(v.nama));
+                            newRow.append($('<td class="text-center">').html(`
+                                <button type="button" class="btn btn-primary" data-id="${v.id}"
+                                data-bs-toggle="modal" data-bs-target="#modal-edit">Edit</button>
+                                <a href="/modal/customer-delete/${v.id}" class="btn btn-danger">Delete</a>
+                            `));
+
+                            $('#tableBody').append(newRow);
+                        });
+
+                    }
+                });
+            }
+
+            // load data di modal edit
             $('#modal-edit').on('show.bs.modal', function(event) {
 
                 // Mengambil data dari button yang diklik, cek data-id
@@ -133,6 +183,67 @@
                         $('#nama').val(response.data.nama);
                         $('#email').val(response.data.email);
                         $('#alamat').val(response.data.alamat);
+                    }
+                });
+            });
+
+            // store data
+            $('#form-tambah').submit(function(e) {
+                e.preventDefault();
+
+                $.ajax({
+                    type: "POST",
+                    url: "{{ url('/modal/customer-store') }}",
+                    data: $(this).serialize(),
+                    dataType: "JSON",
+                    success: function(response) {
+                        reloadTable()
+                        $('#form-tambah')[0].reset()
+                        $('#modal-create').modal('hide')
+                    },
+                    error: function(xhr, status, error) {
+                        $.each(xhr.responseJSON.errors, function (i, v) { 
+                            // menambahkan notifikasi alert dibagian atas 
+                            $('#alert-store').append(`
+                            <div class="alert alert-danger" role="alert" id="store-text">
+                                ${v}
+                            </div>`);                             
+
+                            // notifikasi error di setiap inputan
+                            $("#" + i + "_msg_store").html(v);
+                            $("[id='store_" + i + "']").addClass("is-invalid");
+                        });
+                    }
+                });
+
+            });
+
+            // edit data
+            $('#form-edit').submit(function (e) { 
+                e.preventDefault();
+                
+                $.ajax({
+                    type: "PUT",
+                    url: "{{ url('/modal/customer-update') }}",
+                    data: $(this).serialize(),
+                    dataType: "JSON",
+                    success: function(response) {
+                        reloadTable()
+                        $('#form-edit')[0].reset()
+                        $('#modal-edit').modal('hide')
+                    },
+                    error: function(xhr, status, error) {
+                        $.each(xhr.responseJSON.errors, function (i, v) { 
+                            // menambahkan notifikasi alert dibagian atas 
+                            $('#alert-edit').append(`
+                            <div class="alert alert-danger" role="alert" id="store-text">
+                                ${v}
+                            </div>`);                             
+
+                            // notifikasi error di setiap inputan
+                            $("#" + i + "_msg").html(v);
+                            $("[id='" + i + "']").addClass("is-invalid");
+                        });
                     }
                 });
             });
